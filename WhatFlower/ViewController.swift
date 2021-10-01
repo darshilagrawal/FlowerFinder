195597a294ce337ca44461f4aaad3964361bbfb3
darshilagrawal <darshilagrawal2000@gmail.com>
2021-10-01 22:40:10 +0530

    Indentation
    Flower classifier ML model has been removed due to large file size

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -13,7 +13,7 @@ import SwiftyJSON
 import SDWebImage
 
 class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
-
+    
     let imagePicker=UIImagePickerController()
     let wikipediaURl = "https://en.wikipedia.org/w/api.php"
     
@@ -25,41 +25,40 @@ class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePic
         imagePicker.delegate=self
         imagePicker.sourceType = .photoLibrary
         imagePicker.allowsEditing=true
-        // Do any additional setup after loading the view.
     }
-
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let pickedImage=info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+        if let pickedImage=info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
             
-            guard let ciImage=CIImage(image: pickedImage)else{
+            guard let ciImage=CIImage(image: pickedImage) else {
                 fatalError("Could not convert to CIImage")
             }
             detect(flowerImage: ciImage)
         }
-        
         imagePicker.dismiss(animated: true, completion: nil)
     }
+    
     @IBAction func cameraButtonClicked(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
-       
     }
-    func requestInfo(flowerName:String){
+    
+    func requestInfo(flowerName:String) {
         let parameters : [String:String] = [
-          "format" : "json",
-          "action" : "query",
-          "prop" : "extracts|pageimages",
-          "exintro" : "",
-          "explaintext" : "",
-          "titles" : flowerName,
-          "indexpageids" : "",
-          "redirects" : "1",
+            "format" : "json",
+            "action" : "query",
+            "prop" : "extracts|pageimages",
+            "exintro" : "",
+            "explaintext" : "",
+            "titles" : flowerName,
+            "indexpageids" : "",
+            "redirects" : "1",
             "pithumbsize" : "500"
-          ]
-       
+        ]
+        
         Alamofire.request(wikipediaURl, method: .get, parameters: parameters).responseJSON { (response) in
-            if response.result.isSuccess{
+            if response.result.isSuccess {
                 print("Got the wikipedia Info")
-               print(response)
+                print(response)
                 let flowerJSON:JSON=JSON(response.result.value!)
                 let pageID=flowerJSON["query"]["pageids"][0].stringValue
                 let flowerDescription = flowerJSON["query"]["pages"][pageID]["extracts"].stringValue
@@ -67,34 +66,30 @@ class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePic
                 self.imageView.sd_setImage(with: URL(string: flowerImageURL))
                 self.label.text=flowerDescription
                 self.label.textColor = .black
-                
-                
             }
         }
     }
-    func detect(flowerImage:CIImage){
-        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model)else{
+    
+    func detect(flowerImage:CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
             fatalError("Could Not Load MLModel")
         }
         let request=VNCoreMLRequest(model: model) { (request, error) in
-            guard let results=request.results as? [VNClassificationObservation] else{
+            guard let results=request.results as? [VNClassificationObservation] else {
                 fatalError("Error fetching results")
             }
-            if let firstResult=results.first?.identifier{
+            if let firstResult=results.first?.identifier {
                 self.navigationItem.title=firstResult.capitalized
                 self.requestInfo(flowerName: firstResult)
             }
-            
         }
         let handler=VNImageRequestHandler(ciImage:flowerImage)
         do{
             try handler.perform([request])
         }
         catch{
-         print(error)
+            print(error)
         }
-        
     }
-    
 }
 
